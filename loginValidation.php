75637a303c055c97f39a48bc3618a97a44476a4f
Abhishek Jain <ajj_2396@yahoo.com>
2016-09-30 11:31:03 +0530

    Updated Login Validation File.
    Added Partial SQL Code.

--- loginValidation.php
+++ loginValidation.php
@@ -11,6 +11,7 @@
 	//LOGIN PAGE VARIABLES 
 	$login_username;
 	$login_password;
+	$sql;
 
 	//CONNNECTING TO THE DATABASE 
 	$conn = new mysqli ($servername,$username,$password,$database)
@@ -20,6 +21,14 @@
 		//ENTER THE FIELD DETAILS AND YOU CAN ACCESS THE VALUES.
 		$login_username = $_POST("*****") 
 		$login_password = $_POST("*****")
+
+		if ($conn -> connect_error)
+			die ("Connection Failed:". $conn->connect_error)
+		else
+			$sql = "SELECT * FROM si_reg WHERE login_username='$login_username' AND login_password='$login_password'";
+			$result=mysql_query($sql)		
 	}
+
+
 
  ?>
