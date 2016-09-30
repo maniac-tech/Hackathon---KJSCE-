dc3e5b361df74ec4628193b2b5e621665f48413e
Tejas-Nanaware <Tejas-Nanaware@users.noreply.github.com>
2016-09-30 11:26:25 +0530

    Database connection
    Database Connection

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php 
+	$host="localhost";
+	$username="root";
+	$password="";
+	$database="second_innings";
+	$con=mysqli_connect($host,$username,$password,$database) or die("Couldn't connect to the database");
+?>
