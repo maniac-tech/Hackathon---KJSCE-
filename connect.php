fd227c409a176b06ccc1aba29d7befcaa5026d80
Tejas-Nanaware <Tejas-Nanaware@users.noreply.github.com>
2016-10-01 13:04:14 +0530

    Database is now Hosted!

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
-	$host="localhost";
-	$username="root";
-	$password="";
-	$database="second_innings";
+	$host="sql6.freemysqlhosting.net";
+	$username="sql6138296";
+	$password="vcmR2ELdAB";
+	$database="sql6138296";
 	$con=mysqli_connect($host,$username,$password,$database) or die("Couldn't connect to the database");
 ?>
