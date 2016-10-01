710be8a2ca4d75b9ae70a24ff35dfa0e6b97d009
Tejas-Nanaware <Tejas-Nanaware@users.noreply.github.com>
2016-10-01 13:21:21 +0530

    fixed some errors

--- index.php
+++ index.php
@@ -27,12 +27,12 @@ $(document).ready(function(){
 </body>
 <style type="text/css">
 	body{
-		background-image: url("city.jpg") no-repeat center center fixed;
-		/*background-size: 1400px 800px;*/
-		-webkit-background-size: cover;
+		background-image: url("city.jpg") /*no-repeat center center fixed*/;
+		background-size: 1400px 800px;
+		/*-webkit-background-size: cover;
         -moz-background-size: cover;
         -o-background-size: cover;
-        background-size: cover;
+        background-size: cover;*/
 	}
 	ul {
 		list-style-type: none;
