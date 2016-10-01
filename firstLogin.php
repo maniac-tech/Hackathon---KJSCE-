bc09b237984a9e42f99066e804c1e851f3f73264
Prashant-mahajan <prashantmahajan70@gmail.com>
2016-10-01 15:44:05 +0530

    Update firstLogin.php

--- firstLogin.php
+++ firstLogin.php
@@ -4,6 +4,9 @@
     <title>Geolocation</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   </head>
   <body>
 	  <?php
@@ -71,7 +74,6 @@
       }
       #topMain{
       	color: pink ;
-	margin-top: 55px;
       }
       #map {
         height: 50%;
