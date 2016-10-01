e8b94aa2d12bc861eb4693d58d9f8af94bf63ad5
Abhishek Jain <ajj_2396@yahoo.com>
2016-10-01 13:50:24 +0530

    Update regForm.php

--- regForm.php
+++ regForm.php
@@ -76,4 +76,14 @@
 		</div>
 	</div>
 </body>
+<style type="text/css">
+body{
+		background-image: url("city.jpg") no-repeat center center fixed;
+		/*background-size: 1400px 800px;*/
+		-webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+	}
+</style>
 </html>
